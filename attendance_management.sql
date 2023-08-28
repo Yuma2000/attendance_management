74b4f0e883abe2d34ca02366bf08344698142954
manaka3100 <k21062kk@aitech.ac.jp>
2023-08-28 17:23:06 +0900

    INSERT分追加しました。テーブル名を複数形にしました。

--- attendance_management.sql
+++ attendance_management.sql
@@ -4,13 +4,13 @@ use attendance_managementdb;
 drop user sample@localhost; //ここはエラーでもOK
 CREATE USER 'sample'@'localhost' IDENTIFIED BY 'password';
 grant all privileges ON attendancedb.* TO 'sample'@'localhost';
-DROP TABLE kindergartener;//ここはエラーでもOK
-CREATE TABLE kindergartener(
+DROP TABLE kindergarteners;//ここはエラーでもOK
+CREATE TABLE kindergarteners(
  	id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
 	name VARCHAR(128),
  	PRIMARY KEY(id)
 	);
-CREATE TABLE record(
+CREATE TABLE records(
  	id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
  	
 	child_id SMALLINT,
@@ -20,7 +20,7 @@ CREATE TABLE record(
  	PRIMARY KEY(id)
 	);
 
-CREATE TABLE reply(
+CREATE TABLE replys(
  	id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
  	
 	absence_id SMALLINT,
@@ -29,9 +29,35 @@ CREATE TABLE reply(
  	PRIMARY KEY(id)
 	);
 
-CREATE TABLE childminder(
+CREATE TABLE childminders(
 	id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
 	name VARCHAR(128),
  	PRIMARY KEY(id)
 
 );
+
+insert into kindergarteners values (1, 'いとう あいか');
+insert into kindergarteners values (2, 'いとう はると');
+insert into kindergarteners values (3, 'いのうえ みさと');
+insert into kindergarteners values (4, 'かとう ゆうま');
+insert into kindergarteners values (5, 'きむら こうき');
+insert into kindergarteners values (6, 'ささき そうた');
+insert into kindergarteners values (7, 'さとう たろう');
+insert into kindergarteners values (8, 'すずき さくら');
+insert into kindergarteners values (9, 'たかはし ゆうな');
+insert into kindergarteners values (10, 'たなか けんじ');
+insert into kindergarteners values (11, 'たむら さくら');
+insert into kindergarteners values (12, 'なかがわ りえ');
+insert into kindergarteners values (13, 'なかむら なおみ');
+insert into kindergarteners values (14, 'まつもと たけし');
+insert into kindergarteners values (15, 'みやざき さわこ');
+insert into kindergarteners values (16, 'やまぐち しんじ');
+insert into kindergarteners values (17, 'やまだ みなこ');
+insert into kindergarteners values (18, 'よしだ ゆうた');
+insert into kindergarteners values (19, 'わたなべ ゆきこ');
+insert into kindergarteners values (20, 'わたべ はるか');
+
+insert into childminders values (1, '鈴木 たけし');
+insert into childminders values (2, '田村 えりこ');
+insert into childminders values (3, '中野 ゆかり');
+insert into childminders values (4, '山田 みさき');

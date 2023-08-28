9f6b0749dc9a1c34ef998c7cf40493fa87fe1e33
TAISEI0704 <132648502+TAISEI0704@users.noreply.github.com>
2023-08-28 18:30:25 +0900

    SQL修正(外部キー制約追加)

--- attendance_management.sql
+++ attendance_management.sql
@@ -4,58 +4,61 @@ use attendance_managementdb;
 drop user sample@localhost; //ここはエラーでもOK
 CREATE USER 'sample'@'localhost' IDENTIFIED BY 'password';
 grant all privileges ON attendancedb.* TO 'sample'@'localhost';
-DROP TABLE kindergarteners;//ここはエラーでもOK
-CREATE TABLE kindergarteners(
+-- DROP TABLE kindergarteners;//ここはエラーでもOK
+-- データベース作成後、ここ以降のSQL文を実行
+CREATE TABLE children(
  	id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
 	name VARCHAR(128),
  	PRIMARY KEY(id)
 	);
+
 CREATE TABLE records(
  	id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
- 	
-	child_id SMALLINT,
- 	day date,
+ 	child_id MEDIUMINT UNSIGNED NOT NULL,
+ 	date DATE,
 	status TINYINT(4),
 	absence_reason VARCHAR(128),
- 	PRIMARY KEY(id)
-	);
-
-CREATE TABLE replys(
- 	id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
- 	
-	absence_id SMALLINT,
-	teacher_id SMALLINT,
-	teacher_reply VARCHAR(128),
- 	PRIMARY KEY(id)
+ 	PRIMARY KEY(id),
+	FOREIGN KEY (child_id) REFERENCES children(id)
 	);
 
 CREATE TABLE childminders(
 	id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
 	name VARCHAR(128),
  	PRIMARY KEY(id)
+	);
 
-);
+CREATE TABLE replies(
+ 	id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	recode_id MEDIUMINT UNSIGNED NOT NULL,
+	minder_id MEDIUMINT UNSIGNED NOT NULL,
+	content VARCHAR(128),
+ 	PRIMARY KEY(id),
+	FOREIGN KEY (recode_id) REFERENCES records(id),
+	FOREIGN KEY (minder_id) REFERENCES childminders(id)
+	);
 
-insert into kindergarteners values (1, 'いとう あいか');
-insert into kindergarteners values (2, 'いとう はると');
-insert into kindergarteners values (3, 'いのうえ みさと');
-insert into kindergarteners values (4, 'かとう ゆうま');
-insert into kindergarteners values (5, 'きむら こうき');
-insert into kindergarteners values (6, 'ささき そうた');
-insert into kindergarteners values (7, 'さとう たろう');
-insert into kindergarteners values (8, 'すずき さくら');
-insert into kindergarteners values (9, 'たかはし ゆうな');
-insert into kindergarteners values (10, 'たなか けんじ');
-insert into kindergarteners values (11, 'たむら さくら');
-insert into kindergarteners values (12, 'なかがわ りえ');
-insert into kindergarteners values (13, 'なかむら なおみ');
-insert into kindergarteners values (14, 'まつもと たけし');
-insert into kindergarteners values (15, 'みやざき さわこ');
-insert into kindergarteners values (16, 'やまぐち しんじ');
-insert into kindergarteners values (17, 'やまだ みなこ');
-insert into kindergarteners values (18, 'よしだ ゆうた');
-insert into kindergarteners values (19, 'わたなべ ゆきこ');
-insert into kindergarteners values (20, 'わたべ はるか');
+
+insert into children values (1, 'いとう あいか');
+insert into children values (2, 'いとう はると');
+insert into children values (3, 'いのうえ みさと');
+insert into children values (4, 'かとう ゆうま');
+insert into children values (5, 'きむら こうき');
+insert into children values (6, 'ささき そうた');
+insert into children values (7, 'さとう たろう');
+insert into children values (8, 'すずき さくら');
+insert into children values (9, 'たかはし ゆうな');
+insert into children values (10, 'たなか けんじ');
+insert into children values (11, 'たむら さくら');
+insert into children values (12, 'なかがわ りえ');
+insert into children values (13, 'なかむら なおみ');
+insert into children values (14, 'まつもと たけし');
+insert into children values (15, 'みやざき さわこ');
+insert into children values (16, 'やまぐち しんじ');
+insert into children values (17, 'やまだ みなこ');
+insert into children values (18, 'よしだ ゆうた');
+insert into children values (19, 'わたなべ ゆきこ');
+insert into children values (20, 'わたべ はるか');
 
 insert into childminders values (1, '鈴木 たけし');
 insert into childminders values (2, '田村 えりこ');

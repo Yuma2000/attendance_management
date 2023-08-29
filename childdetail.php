ca06330c2dda0f5d476f6caa64bbc7951e000dcf
Yuma2000 <t.yuma2618@gmail.com>
2023-08-29 18:10:39 +0900

    バグ修正

--- childdetail.php
+++ childdetail.php
@@ -8,14 +8,13 @@ $db = new Database();
 $pdo = $db->connect();
 
 $id = $_GET['id'];
-$child_name = $db->getChildName($id);
+$childName = $db->getChildName($id);
 
 // その月の出欠履歴をデータベースから取得
 $sql = "SELECT date, status, absence_reason FROM records WHERE child_id = ? AND MONTH(date) = MONTH(CURRENT_DATE())";
 $stmt = $pdo->prepare($sql);
 $stmt->execute([$id]);
 $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
 
 <!DOCTYPE html>
@@ -23,36 +22,13 @@ $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <a href="selectchild.php">園児一覧へ戻る</a>
-    
     <title>園児詳細</title>
     <link rel="stylesheet" href="css/childdetail.css">
 </head>
 <body>
-<h2><?= htmlspecialchars($childName, ENT_QUOTES) ?>の出欠履歴</h2>
-<form action="childdetail.php" method="post">
-    <!-- 日付選択 -->
-    <label for="date">日付:</label>
-    <input type="date" id="date" name="date" required>
-    <br><br>
-
-    <!-- 出席/欠席選択 -->
-    <input type="radio" id="attendance" name="status" value="出席" required>
-    <label for="attendance">出席</label>
-    <input type="radio" id="absence" name="status" value="欠席">
-    <label for="absence">欠席</label>
-    <br><br>
-
-    <!-- 欠席理由（欠席を選択した場合のみ入力） -->
-    <label for="reason">欠席理由:</label>
-    <textarea id="reason" name="reason" rows="4" cols="50" placeholder="欠席理由を入力..."></textarea>
-    <br><br>
-
-<label for="date">日付</label>
-<input>
-
 
 <!-- 出欠登録 -->
-<h2><?php echo htmlspecialchars($child_name['name']); ?>：出欠登録</h2>
+<h2><?php echo htmlspecialchars($childName); ?>：出欠登録</h2>
 
 <table border="1">
     <tr>
@@ -67,71 +43,48 @@ $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
         </form>
 
         <?php
-        $status = 2;//出欠状態の初期値
-        $absence_reason = NULL; //初期値としてNULLを設定
+            $status = 2;//出欠状態の初期値
+            $absence_reason = null;  //初期値としてNULLを設定
 
+            if(isset($_POST['status'])) {
+                $status = $_POST['status'];
+                //echo '現在の登録：' . $status . '<br>';
+                $database_attendance = new Database();
+                $record = $database_attendance->record($child_id,$status,$absence_reason);
+            }
 
-        if(isset($_POST['status'])) {
-            $status = $_POST['status'];
-            //echo '現在の登録：' . $status . '<br>';
-            require_once('./database.php');
-            $database_attendance = new Database();
-            $record = $database_attendance -> record($child_id,$status,$absence_reason);
-        }
-
-        if ($status == 1){
-            echo "出席が登録されました．<br>車に気をつけて，元気に登校してね！<br>";
-        }
-
-        if ($status == 2) : ?>
+            if ($status == 1){
+                echo "出席が登録されました．<br>車に気をつけて，元気に登校してね！<br>";
+            }
+        ?>
+        <?php if ($status == 2): ?>
             <form method="post">
                 欠席理由
-        <div>
-            <label for="absence_reason"></label>
-            <textarea id="absence_reason" name="absence_reason" cols="50" rows="10"></textarea>
-        </div>
-        <input type="submit" value="送信"><br>
+            <div>
+                <label for="absence_reason"></label>
+                <textarea id="absence_reason" name="absence_reason" cols="50" rows="10"></textarea>
+            </div>
+            <input type="submit" value="送信"><br>
         <?php endif; ?>
 
         <?php
-
-        if(isset($_POST['absence_reason'])) {
-            $absence_reason = $_POST['absence_reason'];
-            //echo '現在の登録：' . $status . '<br>';
-            if(empty($_POST['absence_reason'])){
-                echo '欠席理由が入力されていません．<br>';
-            }elseif(!empty($_POST['absence_reason'])){
-                require_once('./database.php');
-                $database_attendance = new Database();
-                $record = $database_attendance -> record($child_id,$status,$absence_reason);
-                echo '欠席理由が送信されました．<br>';
+            if(isset($_POST['absence_reason'])) {
+                $absence_reason = $_POST['absence_reason'];
+                if (empty($_POST['absence_reason'])) {
+                    echo '欠席理由が入力されていません．<br>';
+                } elseif (!empty($_POST['absence_reason'])) {
+                    $record = $db->record($child_id,$status,$absence_reason);
+                    echo '欠席理由が送信されました．<br>';
+                }
+            
             }
-           
-        } 
-
-        
-
         ?>
 
     </tr>
-    
-
 </table>
-
 
 <!-- 出欠履歴 -->
-<h2><?php echo htmlspecialchars($child_name['name']); ?>の出欠履歴</h2>
-
-
-<table border="1">
-    <tr>
-        <th>日付</th>
-        <th>出欠</th>
-        <th>欠席理由</th>
-        <th>返信</th>
-    </tr>
-</table>
+<h2><?php echo htmlspecialchars($childName); ?>の出欠履歴</h2>
 
 </body>
 </html>
-

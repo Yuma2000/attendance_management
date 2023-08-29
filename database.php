53f78a13deb58b78556e95383fdbaa24b0c94aca
manaka3100 <k21062kk@aitech.ac.jp>
2023-08-29 13:31:02 +0900

    変更

--- database.php
+++ database.php
@@ -43,17 +43,7 @@ class Database{
         return $recordsWithPresentChildNames;
     }
 
-    //recordsテーブルのデータを全取得＆recordsテーブルのchild_idからchildrenテーブルのnameカラムの値を取得する（出席者用）
-    function all_records_present(){
-        $pdo = $this->connect();
-        $sql = 'SELECT records.*, children.name AS child_name FROM records
-                INNER JOIN children ON records.child_id = children.id WHERE records.status = 1';
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute();
-        $recordsWithPresentChildNames = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
-        return $recordsWithPresentChildNames;
-    }
+    
 
     //recordsテーブルのデータを全取得＆recordsテーブルのchild_idからchildrenテーブルのnameカラムの値を取得する（欠席者用）
     function all_records_absent(){

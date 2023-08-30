5b62596819760e78f2da928eb83f0122b19ef004
Yuma2000 <t.yuma2618@gmail.com>
2023-08-30 19:30:34 +0900

    #41 selectchild.phpの表示内容追加

--- selectchild.php
+++ selectchild.php
@@ -15,12 +15,23 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <title>園児一覧</title>
+    <title>F保育園 / 園児選択</title>
     <link rel="stylesheet" href="css/selectchild.css">
-</head>
+    </head>
 <body>
-    <h3>園児一覧</h3>
-    <table>
+    <div class="header">
+        <div class="header-left">
+            <a href="index.php">トップページに戻る</a>
+        </div>
+        <div class="header-center">
+            F保育園
+        </div>
+        <div class="header-right">
+            保護者としてログイン中
+        </div>
+    </div>
+    <table class="children-table">
+        <caption><h3>園児一覧</h3></caption>
         <tr>
             <th>名前</th>
         </tr>

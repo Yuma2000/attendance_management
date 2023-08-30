b8c072e9e75e251ceb044c5b1f0f754d3a978b84
yoneeeeti <yuto22152@gmail.com>
2023-08-30 14:49:27 +0900

    #31 未提出者の関数，表を作成．テーブルを横並びにして表示．

--- management.php
+++ management.php
@@ -19,8 +19,11 @@ $database_absent = new Database();
 //欠席者を取得
 $records_ab = $database_absent -> all_records_absent();
 
-$countPre = 0;
-$countAd = 0;
+/* 未提出者を取得 */
+$database_yet = new Database();
+$records_yet = $database_yet -> all_records_yet();
+
+
 ?>
 
 <!DOCTYPE html>
@@ -34,29 +37,38 @@ $countAd = 0;
     <h3>生徒出欠管理画面</h3>
     <h3>本日の日付： <tr>  <td><?= date('Y/m/d'); ?></td></tr></h3>
 
-    <table>
-        <tr>
-            <th>出席</th>
-            <?php foreach($records_pre as $records_pre2){ ?>
-            <?php $countPre++; ?>
-        <tr>
-            <td><a href="response.php?id=<?php print($records_pre2['child_id']) ?>" name="sentaku"><?php print($records_pre2['child_name']) ?></a></td>
-        </tr>
+    <div class="table-content">
+        <div class="attendance">
+            <table border="1">
+                <tr>
+                    <th>出席</th>
+                    <?php foreach($records_pre as $records_pre2){ ?>
+                <tr>
+                    <td><a href="response.php?id=<?php print($records_pre2['child_id']) ?>" name="sentaku"><?php print($records_pre2['child_name']) ?></a></td>
+                </tr>
+                <?php } ?>
+                    <th>欠席</th>
+                    <?php foreach($records_ab as $records_ab2){ ?>
+                <tr>
+                    <td><a href="response.php?id=<?php print($records_ab2['child_id']) ?>" name="sentaku"><?php print($records_ab2['child_name']) ?></a></td>
+                </tr>
+                <?php } ?>
+                </tr>
+            </table> 
+        </div>
+
+        <div class="yet">
+            <table border="1">
+            <th>未提出</th>
+                    <?php foreach($records_yet as $records_yet2){ ?>
+                <tr>
+                    <td><a href="response.php?id=<?php print($records_yet2['child_id']) ?>" name="sentaku"><?php print($records_yet2['child_name']) ?></a></td>
+                </tr>
             <?php } ?>
-            <th>欠席</th>
-            <?php foreach($records_ab as $records_ab2){ ?>
-            <?php $countAd++; ?>
-        <tr>
-            <td><a href="response.php?id=<?php print($records_ab2['child_id']) ?>" name="sentaku"><?php print($records_ab2['child_name']) ?></a></td>
-        </tr>
-            <?php } ?>
-        
-        </tr>
-    </table>
-    <div>
-        <p>出席者：<?= $countPre ?> 人</p>
-        <p>欠席者：<?= $countAd ?> 人</p>
+            </table>
+        </div>
     </div>
     
+
 </body>
 </html>

b70eab91b520b41afdcc90640b662f11557f0828
Yuma2000 <t.yuma2618@gmail.com>
2023-08-30 20:38:47 +0900

    #43 トップページをレスポンシブ化

--- index.php
+++ index.php
@@ -15,38 +15,40 @@
             <h1>F保育園</h1>
         </div>
     </header>
-    <div class="signin">
-        <form method="POST" action="">
-            <h1>出欠管理アプリ</h1>
-            <div class="guardian-or-childminder">
-                <div>
-                    <a href="selectchild.php" class="btn-link">保護者ログイン</a>
+    <div class="content">
+        <div class="signin">
+            <form method="POST" action="">
+                <h1>出欠管理アプリ</h1>
+                <div class="guardian-or-childminder">
+                    <div>
+                        <a href="selectchild.php" class="btn-link">保護者ログイン</a>
+                    </div>
+                    <div>
+                        <a href="management.php" class="btn-link">保育士ログイン</a>
+                    </div>
                 </div>
-                <div>
-                    <a href="management.php" class="btn-link">保育士ログイン</a>
-                </div>
-            </div>
-        </form>
-    </div>
-    <div class="intro">
-        <h2>園児の出欠管理を簡単に。</h2>
-        <p>F保育園の出欠管理アプリは、園児の出欠登録や履歴の確認を手軽に行えます。</p>
-    </div>
-    <div class="contact-info">
-        <p>住所: 東京都新宿区〇〇〇〇-〇〇〇〇</p>
-        <p>電話番号: 03-1234-5678</p>
-        <p>メール: contact@f-nursery.example.com</p>
-    </div>
-    <div class="news">
-        <h2>お知らせ</h2>
-        <ul>
-            <li><a href="#">新型コロナウイルス対策について</a></li>
-            <li><a href="#">年間行事予定の変更点</a></li>
-            <li><a href="#">新しい先生の紹介</a></li>
-        </ul>
-    </div>
-    <div class="official-site">
-        <a href="https://f-nursery.example.com">F保育園の公式サイトへ</a>
+            </form>
+        </div>
+        <div class="intro">
+            <h2>お休みの連絡、出欠管理を簡単に。</h2>
+            <p>F保育園の出欠管理アプリは、忙しい保護者と保育士をサポートします。<br>園児の出欠登録や履歴の確認をオンラインで手軽に行えます。</p>
+        </div>
+        <div class="contact-info">
+            <p>住所: 東京都新宿区〇〇〇〇-〇〇〇〇</p>
+            <p>電話番号: 03-1234-5678</p>
+            <p>メール: contact@f-nursery.example.com</p>
+        </div>
+        <div class="news">
+            <h2>お知らせ</h2>
+            <ul>
+                <li><a href="#">新型コロナウイルス対策について</a></li>
+                <li><a href="#">年間行事予定の変更点</a></li>
+                <li><a href="#">新しい先生の紹介</a></li>
+            </ul>
+        </div>
+        <div class="official-site">
+            <a href="https://f-nursery.example.com">F保育園の公式サイトはこちら</a>
+        </div>
     </div>
 
     <footer>

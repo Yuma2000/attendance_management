8638740c7302fc1bb71b0b4507696f81caec82c8
manaka3100 <k21062kk@aitech.ac.jp>
2023-08-31 15:25:56 +0900

    #78 実装しました

---
+++ loginform.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>出欠れんらくん</title>
+</head>
+<body>
+    <form action="childdetail.php" method="GET">
+        <label for="">生徒ID</label><br>
+        <input type="text" name="user_name" required="required"><br>
+        <label for="">パスワード</label><br>
+        <input type="text" name="password" required="required"><br>
+        <input type="submit" value="ログイン"> 
+    </form>
+    
+</body>
+</html>
+
